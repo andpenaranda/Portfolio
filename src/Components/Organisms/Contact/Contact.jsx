b6f0f37887b4eb908979101a1642329bc04ffa43
ANDREA <122313137+andpenaranda@users.noreply.github.com>
2023-02-20 18:26:47 -0500

    Add information to contact page

--- src/Components/Organisms/Contact/Contact.jsx
+++ src/Components/Organisms/Contact/Contact.jsx
@@ -4,6 +4,11 @@ export const Contact = () => {
   return (
     <div>
       <p className={styles.titleContact}>Contact.</p>
+      <p className={styles.paragraphContact}>
+        Let's keep in touch, I'm not <br /> currently looking for any new <br />
+        opportunities but don't think twice <br /> my inbox is allways open.
+      </p>
+      <button className={styles.button} type="button">Say hi!</button>
     </div>
   );
 };
